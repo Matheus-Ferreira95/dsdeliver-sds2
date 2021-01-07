a34703d395f9f13d5b834d0ce4cb2b9f86292df2
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2021-01-07 17:45:41 -0300

    POST /ORDERS

--- backend/src/main/java/com/matheusf/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/matheusf/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.matheusf.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheusf.dsdeliver.dto.OrderDTO;
 import com.matheusf.dsdeliver.services.OrderService;
@@ -23,5 +27,11 @@ public class OrderController {
 		List<OrderDTO> list = orderService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = orderService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- backend/src/main/java/com/matheusf/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/matheusf/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.matheusf.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.matheusf.dsdeliver.dto.OrderDTO;
+import com.matheusf.dsdeliver.dto.ProductDTO;
 import com.matheusf.dsdeliver.entities.Order;
+import com.matheusf.dsdeliver.entities.OrderStatus;
+import com.matheusf.dsdeliver.entities.Product;
 import com.matheusf.dsdeliver.repositories.OrderRepository;
+import com.matheusf.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,23 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = orderRepository.findOrdersWithProducts();
 		return list.stream().map(order -> new OrderDTO(order)).collect(Collectors.toList());
 	}	
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
+	}
 }
