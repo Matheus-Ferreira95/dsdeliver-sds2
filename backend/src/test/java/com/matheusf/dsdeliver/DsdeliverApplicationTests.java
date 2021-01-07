6c71e0e9335324a5865269251864d83f6f5faf7d
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2021-01-07 14:35:13 -0300

    project created

---
+++ backend/src/test/java/com/matheusf/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusf.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
